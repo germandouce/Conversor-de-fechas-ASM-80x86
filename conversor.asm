77bc73fedbc7c84e8f6195ca657de6dfde2bcda8
German Douce <germandouce@gmail.com>
2022-09-13 02:38:12 -0300

    subo primeros pasos de archivo en asm e ideas en txt

--- conversor.asm
+++ conversor.asm
@@ -1,22 +1,37 @@
+;- Trabajo práctico Nro 20
+;- Douce, Germán A. - 106001
+;** Conversor de Fechas **
+;**************************************************************************************************
+;Desarrollar un programa en assembler Intel 80x86 que permita convertir fechas válidas con el 
+;formato DD/MM/AAAA a formato romano y/o juliano (DDD/AA); también debe permitir su inversa.
+;Ejemplo:
+; - Formato Fecha (DD/MM/AAAA): 14/10/2020
+; - Formato Romano: XIV / X / MMXX
+; - Formato Juliano (DDD/AA): 288/20
+;****************************************************************************************************
+;Comentarios y suposiciones del enunciado:
+;
+;
+;
 
-;Validar fechas en rangos esperados
-;y tambien como corte de suma de simbolos
-;             finLoop, "N"
-;mov          byte[finNum], "S" ;pongo una S para dar el okey de q se ingreso bien
-;call            validarFyC ;validamos. en esa rutina modificamos el valor de 'inputValido'
-;cmp             word[FinNum], "N" ;N - no valido / S - valido
-;je              ingresoDatos
+global main
+extern printf
+extern puts
+extern sscanf 
+extern gets
+
+section     .data
 
 
-; mov             byte[inputValido], "N"; Le coloco un no a la var. es como un false antes del ciclo
-;        ;chequeo q haya casteado los 2 valores enteros ( por ej 3 E daria 1)
-;        cmp             rax,2 ;
-;        jl              invalido
 
 
-;concatenar un simbolo a la vez en el loop
-; en vez de 4 va 1
-; mov eax, [fizz]
-; mov [buffer], eax
-; mov eax, [buzz]
-; mov [buffer+4], eax
+section     .bss
+
+
+
+section     .text
+
+main:
+
+
+ret
